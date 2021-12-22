a46308672392f52d82b6fa41e5a9ce66384d2f77
ImJunHong <tensai3355@gmail.com>
2021-12-22 14:32:34 +0900

    :bug: Fix incorrect variable name

--- js/board.js
+++ js/board.js
@@ -19,46 +19,50 @@ const board = {
     const queue = [start];
     let tag;
 
-    while (stack.length) {
+    while (queue.length) {
       tag = queue.shift();
       if (tag.elem === target) break;
-      if (!visited.has(node)) {
-        visited.set(elem, null);
-        queue.push(...elem.children);
+      if (!visited.has(tag)) {
+        visited.set(tag, null);
+        queue.push(...tag.children);
       }
     }
     return tag;
-  },
-
-  clickHandler(event) {
-    if (this.ready) {
-      const { offsetX, offsetY } = event;
-      this.setPosByGrid(this.ready, offsetX, offsetY);
-      this.ready.setState("display");
-    } else {
-      const { target } = event;
-      this.selected = this.searchElem(target);
-      this.selected.setState("selected");
-    }
-  },
-
-  mouseoverHandler({ offsetX, offsetY }) {
-    if (!this.ready) this.ready.setState("ready");
-    this.setPosByGrid(this.ready, offsetX, offsetY);
-  },
-
-  mousemoveHandler() {
-    if (!this.ready) return;
-    this.setPosByGrid(this.ready, offsetX, offsetY);
-  },
-
-  mouseoutHandler() {
-    if (!this.ready) return;
-    this.ready.setState("none");
   }
 };
 
-board.elem.addEventListener("click", board.clickHandler);
-board.elem.addEventListener("mouseover", board.mouseoverHandler);
-board.elem.addEventListener("mousemove", board.mousemoveHandler);
-board.elem.addEventListener("mouseout", board.mouseoutHandler);
+const clickHandler = (event) => {
+  const { ready } = board;
+  if (ready) {
+    const { offsetX, offsetY } = event;
+    board.setPosByGrid(ready, offsetX, offsetY);
+    ready.setState("display");
+  } else {
+    const { target } = event;
+    board.selected = board.searchElem(target);
+    board.selected.setState("selected");
+  }
+};
+
+const mouseoverHandler = ({ offsetX, offsetY }) => {
+  const { ready } = board;
+  if (!ready) return;
+  body.setPosByGrid(ready, offsetX, offsetY);
+};
+
+const mousemoveHandler = () => {
+  const { ready } = board;
+  if (!ready) return;
+  board.setPosByGrid(ready, offsetX, offsetY);
+};
+
+const mouseoutHandler = () => {
+  const { ready } = board;
+  if (!ready) return;
+  ready.setState("none");
+};
+
+board.elem.addEventListener("click", clickHandler);
+board.elem.addEventListener("mouseover", mouseoverHandler);
+board.elem.addEventListener("mousemove", mousemoveHandler);
+board.elem.addEventListener("mouseout", mouseoutHandler);
