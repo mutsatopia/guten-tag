57eda56af529105c1def67362252383df36f9b58
Tesseract <tensai3355@gmail.com>
2021-12-23 18:13:50 +0900

    :sparkles: Add functions to util.js

--- js/util.js
+++ js/util.js
@@ -1,6 +1,34 @@
+const trim = (value, unit) => value - (value % unit);
+
+const setElemSize = (elem, width, height) => {
+  if (typeof width === "string") {
+    elem.style.width = width;
+  } else {
+    elem.style.width = `${width}px`;
+  }
+  
+  if (typeof height === "string") {
+    elem.style.height = height;
+  } else {
+    elem.style.height = `${height}px`;
+  }
+};
+
+const setElemPos = (elem, x, y) => {
+  if (typeof x === "string") {
+    elem.style.left = x;
+  } else {
+    elem.style.left = `${x}px`;
+  }
+
+  if (typeof y === "string") {
+    elem.style.top= y;
+  } else {
+    elem.style.top = `${y}px`;
+  }
+};
+
 const setElemStyle = (elem, width, height, x, y) => {
-  elem.style.width = `${width}px`;
-  elem.style.height = `${height}px`;
-  elem.style.left = `${x}px`;
-  elem.style.top = `${y}px`;
+  setElemSize(elem, width, height);
+  setElemPos(elem, x, y);
 };
