469ce50f2f0a8bd61b0c0f2ee536d3a6c78bc4f7
Tesseract <tensai3355@gmail.com>
2021-12-23 15:44:50 +0900

    :sparkles: Add util.js

---
+++ js/util.js
@@ -0,0 +1,6 @@
+const setElemStyle = (elem, width, height, x, y) => {
+  elem.style.width = `${width}px`;
+  elem.style.height = `${height}px`;
+  elem.style.left = `${x}px`;
+  elem.style.top = `${y}px`;
+};
