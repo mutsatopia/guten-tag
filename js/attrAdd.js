6667ee0f166806dc570ee6e0273511b628c910af
dwhddnjs <per1215@naver.com>
2021-12-29 10:54:43 +0900

    :white_check_mark: Fix code and delete attr from data.js

--- js/attrAdd.js
+++ js/attrAdd.js
@@ -1,63 +1,63 @@
-const addBtn = document.querySelector('.btn-attr-add')
-const attrContainer = document.querySelector('.attr-item-containers')
-const attrInput = document.querySelector('.input-attr-user-custom')
-const show = document.querySelector('.wrap-attr-menu')
+const addBtn = document.querySelector(".btn-attr-add");
+const attrContainer = document.querySelector(".attr-item-containers");
+const attrInput = document.querySelector(".input-attr-user-custom");
+const show = document.querySelector(".wrap-attr-menu");
 
-addBtn.addEventListener('click', customInput)
-attrInput.addEventListener('focusout', addAttr)
+addBtn.addEventListener("click", customInput);
+attrInput.addEventListener("focusout", addAttr);
 
 
 const attrArr = [
   {
-    title: 'href',
-    attr: 'www.sival.com'
+    title: "href",
+    attr: "www.gutentag.com"
   },
   {
-    title: 'type',
-    attr: 'button'
+    title: "type",
+    attr: "button"
   }
-]
+];
 
 function customInput(){
-  show.style.display='flex'
-}
+  show.style.display="flex"
+};
 
 function addAttr() {
-  const elName = document.querySelector('.input-attr-name')
-  const attr = document.querySelector('.input-attr-user-custom')
+  const elName = document.querySelector(".input-attr-name");
+  const attr = document.querySelector(".input-attr-user-custom");
   attrArr.push({
     title: elName.value,
     attr: attr.value
-  })
+  });
 
   while(attrContainer.hasChildNodes()){
-    attrContainer.removeChild(attrContainer.firstChild)
-  }
+    attrContainer.removeChild(attrContainer.firstChild);
+  };
 
-  AttrRender(attrArr)
-  elName.value = null
-  attr.value = null
-  show.style.display='none'
-}
+  attrRender(attrArr);
+  elName.value = null;
+  attr.value = null;
+  show.style.display="none";
+};
 
-function AttrRender(arr) {
+function attrRender(arr) {
   arr.forEach((el) => {
-    const container = document.createElement('div')
-    container.className = 'attr-list-container'
+    const container = document.createElement("div");
+    container.className = "attr-list-container";
 
-    const title = document.createElement('span')
-    title.textContent = el.title
-    title.className = 'attr-list-title'
+    const title = document.createElement("span");
+    title.textContent = el.title;
+    title.className = "attr-list-title";
 
-    const desc = document.createElement('span')
-    desc.textContent = el.attr
-    desc.className = 'attr-list-desc'
+    const desc = document.createElement("span");
+    desc.textContent = el.attr;
+    desc.className = "attr-list-desc";
 
-    container.appendChild(title)
-    container.appendChild(desc)
-    attrContainer.appendChild(container)
-  })
-}
+    container.appendChild(title);
+    container.appendChild(desc);
+    attrContainer.appendChild(container);
+  });
+};
 
-AttrRender(attrArr)
+attrRender(attrArr)
 
--- js/data.js
+++ js/data.js
@@ -2,236 +2,189 @@ const tagData = [
   {
     tagName: "div",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "p",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "span",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "a",
     keyword: ["inline"],
-    attr: ['href'],
   },
   {
     tagName: "img",
     keyword: ["inline"],
-    attr: ['src', 'alt'],
   },
   {
     tagName: "ul",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "ol",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "li",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "header",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "nav",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "main",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "aside",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "section",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "article",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "footer",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "summary",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "details",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "figure",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "figcaption",
     keyword: ["block", "semantic"],
-    attr: [],
   },
   {
     tagName: "mark",
     keyword: ["inline", "semantic"],
-    attr: [],
   },
   {
     tagName: "h1",
     keyword: ["block"],
-    attr: ['type'],
   },
   {
     tagName: "h2",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "h3",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "h4",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "h5",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "h6",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "fieldset",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "form",
     keyword: ["block"],
-    attr: ['action'],
   },
   {
     tagName: "label",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "input",
     keyword: ["block"],
-    attr: ['type'],
   },
   {
     tagName: "button",
     keyword: ["block"],
-    attr: ['type', 'onclick'],
   },
   {
     tagName: "select",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "strong",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "b",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "i",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "em",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "u",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "textarea",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "address",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "small",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "dl",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "dt",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "dd",
     keyword: ["block"],
-    attr: [],
   },
   {
     tagName: "iframe",
     keyword: ["block"],
-    attr: ['src'],
   },
   {
     tagName: "sup",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "abbr",
     keyword: ["inline"],
-    attr: [],
   },
   {
     tagName: "code",
     keyword: ["inline"],
-    attr: [],
   },
 ];
