858c8c91e7a1d64c78b96075291b2272aa4d39f5
whara123 <whara123@gmail.com>
2022-01-01 02:20:17 +0900

    :fire: Delete theme js

--- js/theme.js
+++
@@ -1,6 +0,0 @@
-const palette = document.querySelector("#btn-palette");
-const paletteOpenBtn = document.querySelector(".btn-theme-open");
-
-paletteOpenBtn.addEventListener("click",()=>{
-    palette.classList.toggle("palette");
-})
